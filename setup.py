a76964234d30f69d141997f63d1e422b1a29f5a0
Furkan Dinçer <68972804+frkndncr@users.noreply.github.com>
2024-10-28 15:13:36 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,24 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='proje_adı',  # Proje adınızı buraya yazın
-    version='0.1',
+    name='C4Guard',
+    version='0.1.0',
+    description='A web application for searching files and monitoring system resources.',
+    author='Your Name',
+    author_email='your_email@example.com',
+    url='https://github.com/frkndncr/c4guard',
     packages=find_packages(),
     install_requires=[
-        'aiohappyeyeballs==2.4.3',
-        'aiohttp==3.10.8',
-        'beautifulsoup4==4.8.0',
-        'Flask==3.0.3',
-        'numpy==1.26.4',
-        # Diğer bağımlılıkları buraya ekleyin
+        'Flask>=1.1.2',
+        'aiohttp>=3.7.4',
+        'beautifulsoup4>=4.9.3',
+        'numpy>=1.19.5',
+        'aiohappyeyeballs>=1.0.3',
     ],
-    entry_points={
-        'console_scripts': [
-            'proje_adı = app:main',  # app.py dosyanızda bulunan ana fonksiyonu belirtin
-        ],
-    },
-    python_requires='>=3.6',  # Gereken Python sürümü
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
 )
